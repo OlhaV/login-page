a9a02587da511ef14ad69684fd7b8a28cce09756
Olga Vasyletska <olga.vasyletska@netcracker.com>
2018-11-01 12:57:20 +0200

    refactoring menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,58 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {MenuModel} from "../../models/menu.model";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
   @Input() authorized: boolean = false;
   @Output() logoutClick = new EventEmitter();
 
-  constructor() { }
+  public _menuItems: MenuModel[];
 
-  ngOnInit() {
+  constructor() {
+    this.initMenuItems();
+  }
+
+  private initMenuItems(): void {
+    this._menuItems = [
+      {
+        value: 'Home',
+        routerLink: [''],
+        routerLinkActive: ['active-link'],
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        value: 'About',
+        routerLink: ['about'],
+        routerLinkActive: ['active-link'],
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        value: 'Order Data',
+        routerLink: ['order-data'],
+        routerLinkActive: ['active-link'],
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        value: 'User',
+        routerLink: ['user', '12'],
+        routerLinkActive: ['active-link'],
+        routerLinkActiveOptions: { exact: true },
+      },
+      {
+        value: 'Logout',
+        routerLink: ['logout'],
+        routerLinkActive: ['active-link'],
+        routerLinkActiveOptions: { exact: true },
+        action: () => {
+          this.logoutClick.emit()
+        }
+      },
+    ]
   }
 
 }
---
+++ src/app/models/menu.model.ts
@@ -0,0 +1,7 @@
+export class MenuModel {
+  value: string;
+  routerLink: string[];
+  routerLinkActive: string[];
+  routerLinkActiveOptions?: { exact: boolean };
+  action?: () => {};
+}
